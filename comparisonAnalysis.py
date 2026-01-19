Fixed Graph Vis Scaling

comparisonAnalysis.py
@@ -4,31 +4,39 @@ from matplotlib import animation
 import numpy as np
 import math
 
-with open('savedDataTest.pkl', 'rb') as f:
+with open('savedData20cm.pkl', 'rb') as f:
     data = pickle.load(f)
-with open('savedDataNone.pkl', 'rb') as f:
-    noiseData = pickle.load(f)
 
-print(len(data[0][8]))
-exit()
+#with open('savedDataNone.pkl', 'rb') as f:
+#    noiseData = pickle.load(f)
+#
+#print(len(data[0][8]))
+#exit()
+#
+#noiseProfile = [0 for val in noiseData[0][8]]
+#for val in noiseData:
+#    for i in range(len(val[8])):
+#        noiseProfile[i] += val[8][i]
+#noiseProfile = [val/len(noiseData) for val in noiseProfile]
+#
+#
+#dataDenoised = [[data[i][8][j] - noiseProfile[j] for j in range(len(data[i][8]))] for i in range(len(data))]
+#x = np.linspace(0, 2.41, len(data[i][8]))
+##plt.plot(x, noiseProfile)
+#plt.plot(x, data[0][8])
+#plt.plot(x, dataDenoised[0])
+#plt.show()
+#
+#
+#
+#exit()
 
-noiseProfile = [0 for val in noiseData[0][8]]
-for val in noiseData:
-    for i in range(len(val[8])):
-        noiseProfile[i] += val[8][i]
-noiseProfile = [val/len(noiseData) for val in noiseProfile]
-
-
-dataDenoised = [[data[i][8][j] - noiseProfile[j] for j in range(len(data[i][8]))] for i in range(len(data))]
-x = np.linspace(0, 2.41, len(data[i][8]))
-#plt.plot(x, noiseProfile)
-plt.plot(x, data[0][8])
-plt.plot(x, dataDenoised[0])
-plt.show()
-
-
-
-exit()
+dataLinScale = []
+dataLogScale = []
+for i in range(len(data)):
+    x = np.linspace(0, 2.41, len(data[i][8]))
+    dataLinScale.append([(32.0/float(len(x))) * (2**(val/512.0)) for val in data[i][8]])
+    dataLogScale.append([(val*20.0*math.log10(2.0))/512.0 + 20.0*math.log10(32.0/float(len(x))) for val in data[i][8]])
 
 # Create figure with 4 subplots (2x2 grid)
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 10))
@@ -50,7 +58,16 @@ ax4.set_title('All Lines Combined')
 # Set axis limits for all subplots
 for ax in [ax1, ax2, ax3, ax4]:
     ax.set_xlim(0, 2.41)
-    ax.set_ylim(min([min(d[8]) for d in data]) / y_scale, max([max(d[8]) for d in data]))
+dataMax = max([max(d[8]) for d in data])
+dataMin = min([min(d[8]) for d in data])
+dataLinMax = max([max(d) for d in dataLinScale])
+dataLinMin = min([min(d) for d in dataLinScale])
+dataLogMax = max([max(d) for d in dataLogScale])
+dataLogMin = min([min(d) for d in dataLogScale])
+ax1.set_ylim(dataMin, dataMax)
+ax2.set_ylim(dataLinMin, dataLinMax)
+ax3.set_ylim(dataLogMin, dataLogMax)
+ax4.set_ylim(min([dataMin,dataLogMin,dataLinMin]), max([dataMax,dataLogMax,dataLinMax]))
 
 # Initialization function
 def init():
@@ -62,11 +79,10 @@ def init():
     line4_log.set_data([], [])
     return line1, line2, line3, line4_orig, line4_lin, line4_log
 
-# Animation function (i is the frame number)
 def animate(i):
     x = np.linspace(0, 2.41, len(data[i][8]))
-    linScale = [(32.0/float(len(x))) * (2**(val/512.0)) for val in data[i][8]]
-    logScale = [(val*20.0*math.log10(2.0))/512.0 + 20.0*math.log10(32.0/float(len(x))) for val in data[i][8]]
+    linScale = [val for val in dataLinScale[i]]
+    logScale = [val for val in dataLogScale[i]]
     y = [val for val in data[i][8]]
     
     # Update individual subplot lines
@@ -79,15 +95,9 @@ def animate(i):
     line4_lin.set_data(x, linScale)
     line4_log.set_data(x, logScale)
     
-    # Update frame counter on the main figure
-    fig.suptitle(f'Frame {i}')
-    
     return line1, line2, line3, line4_orig, line4_lin, line4_log
 
-# Adjust layout to prevent overlap
 plt.tight_layout()
-
-# Create animation
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                               frames=len(data), interval=100, blit=True)
 plt.show()